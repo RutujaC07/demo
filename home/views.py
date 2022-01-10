7e9aa379ad0b1769a69de01142325cd0e3f7f44c
RutujaC07 <RutujaC07>
2022-01-10 15:57:51 +0530

    New commit

---
+++ home/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import UserManager
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
+
+# Create your views here.
+def index(request):
+    print(request.user)
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request, "index.html")
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username,password)
+        #check user cred is correct
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect("/")
+        else:
+            # No backend authenticated the credentials
+            return render(request, "login.html")
+    return render(request, "login.html")
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login") 
